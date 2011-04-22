6f80d43daa65e0938f758f62b172151357696da8
Kevin Charter <kevin.charter@acm.org>
2011-04-22 01:38:51 +0000

    Fix lexer action for hexadecimal long literals.
    The lexer action for hexadecimal long literals of the form
    
     0[xX] hexdigit+ [lL]
    
    was building an IntTok instead of a LongTok. I've fixed this, and
    modified the HUnit tests so that for an Integer value they test
    
     - lexing IntToks using decimal, octal, and hexadecimal
       representations
    
     - lexing LongToks using decimal, octal, and hexadecimal
       representations
    
    These tests demonstrated the original bug. Now the bug is gone.
    
    The QuickCheck tests didn't find this bug because they don't generate
    input text with octal and hexadecimal integer literals. I found this
    by doing some tests using real Java source files that included
    hexadecimal long literals (the test code will be forthcoming in a
    future patch).
    
    I'll have to modify the QuickCheck tests to allow varying the input
    representation for numbers.

--- test/LexerTest.hs
+++ test/LexerTest.hs
@@ -1,6 +1,7 @@
 module Main where
 
 import Test.HUnit
+import Text.Printf (printf)
 import Language.Java.Lexer
 
 import qualified LexerQCTest as LQCT
@@ -10,19 +11,31 @@ data LexerTest = LexerTest
                     [Token] -- expected result
 
 lexerTests :: [LexerTest]
-lexerTests = [
-    LexerTest "23"
-    [IntTok 23]
-  , LexerTest "0x1"
-    [IntTok 1]
-  , LexerTest "0x01"
-    [IntTok 1]
-  , LexerTest "0x17"
-    [IntTok 23]
-  , LexerTest "\"\\\\\" \"\""
-    [StringTok "\\", StringTok ""]
+lexerTests = concat [
+    integerTests 0
+  , integerTests 1
+  , integerTests 23
+  , integerTests 52
+  , [ LexerTest "\"\\\\\" \"\""
+      [StringTok "\\", StringTok ""] ]
   ]
 
+-- | Generates integer lexing tests using decimal, octal, and
+-- hexadecimal representations of @int@ and @long@ literals.
+integerTests :: Integer -> [LexerTest]
+integerTests expectedValue =
+  concat [ intAndLongs decimalRep,
+           intAndLongs octalRep,
+           intAndLongs hexRep ]
+  where intAndLongs rep = [ LexerTest rep [intToken],
+                            LexerTest (rep ++ "l") [longToken],
+                            LexerTest (rep ++ "L") [longToken] ]
+        decimalRep = printf "%d"   expectedValue
+        octalRep   = printf "0%o"  expectedValue
+        hexRep     = printf "0x%x" expectedValue
+        intToken   = IntTok  expectedValue
+        longToken  = LongTok expectedValue
+  
 main :: IO ()
 main = do
   _ <- runTestTT $ TestList $ map lexerTestToTest lexerTests
