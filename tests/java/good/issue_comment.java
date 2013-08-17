0d1dcf3e557788cf5911d6fbe5677c3f0facf21e
Vincent Hanquez <vincent@snarc.org>
2013-08-17 08:24:26 +0100

    add some tests and bump version to 2.5

---
+++ tests/java/good/issue_comment.java
@@ -0,0 +1,37 @@
+public class MyTest
+{
+    /**
+     * Constructor for objects of class MyTest
+     * @param name This is the number
+     * @param city This is the city
+     */
+    public MyTest(String name, String city)
+    {
+  
+    } // end of MyTest
+    
+    /**
+
+    /**
+     * Raise the bar.
+     */
+    public String up(String x)
+    {
+        String result;
+        if (x.equals("")) {
+            result = "a";
+        }
+        else {
+            result = "b";
+        }
+        HashSet<Integer> set = new Has<X>();
+        char ch = result.charAt(0);
+        int i = ch;
+        for (Integer eachNum : set)
+        {
+            if (eachNum < 100) {}
+        }
+        return result;
+    }
+
+}
---
+++ tests/java/good/nestedComment.java
@@ -0,0 +1,7 @@
+/** this is a nested
+/**
+ */
+
+public class Comment
+{
+}
