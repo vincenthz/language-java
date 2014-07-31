36b39284411b00c96bf93f666ba2f90ecc186dbf
Losich Alexey <ALosich@qulix.com>
2014-07-31 15:08:55 +0300

    Fix miscMath4 test.
    The problem with this test is that there semicolon was missed in the test
    itself.

--- tests/java/good/miscMath4.java
+++ tests/java/good/miscMath4.java
@@ -2,7 +2,7 @@ import java.util.Random;
 class MiscMath<T extends Number>{
 	Collection<Number> fromArray(Number[] na) {
 		for (Number n : na) {
-			cn.add(n)
+                   cn.add(n);
 		}
 		Collection<Number> cn = new ArrayList<Number>();
 	}
