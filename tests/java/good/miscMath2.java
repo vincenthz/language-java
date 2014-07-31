3ac4d23e1c68933b047782f4aa33a55fde11d5db
Losich Alexey <ALosich@qulix.com>
2014-07-31 15:12:55 +0300

    Fix for miscMath2 test.
    In this test semicolon was missing.

--- tests/java/good/miscMath2.java
+++ tests/java/good/miscMath2.java
@@ -3,7 +3,7 @@ class MiscMath<T extends Number>{
 	Collection<Number> fromArray(Number[] na) {
 		Collection<Number> cn = new ArrayList<Number>();
 		for (Number n : na) {
-			cn.add(n)
+                   cn.add(n);
 		}
 		return cn;
 	}
